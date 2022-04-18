8a9433cbf0dd5698e9fd0bbe1ba25527c83882fd
Edgar Rostro <Edgar Rostro>
2022-04-18 17:33:01 -0500

    actualizar repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { useState } from "react";
+import { suma, resta, ComponenteSimple } from "./funciones/funcionesSimples";
+export default function App() {
+  const [conteo, funcionConteo] = useState(0);
+  const [cadenas, funcionCadena] = useState("cadena");
+  const [arreglos, funcionArreglo] = useState([
+    { nombre: "Ruben" },
+    { nombre: "Sergio" },
+  ]);
+  const [objeto, funcionObjeto] = useState({ nombre: "Ruben" });
+
+  return (
+    <div>
+      
+      {suma()}
+      
+      <ComponenteSimple />
+      
+      <button onClick={() => funcionConteo(conteo + 1)}>Sumatoria</button>
+    </div>
+  );
+}
